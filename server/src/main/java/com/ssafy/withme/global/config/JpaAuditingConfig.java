0ff2b155dab0e804d742bd253b58ee73296fc9fa
jinwoo <zvzv9808@gmail.com>
2024-07-15 13:58:36 +0900

    fix: 패키징 구조 수정

--- server/src/main/java/com/ssafy/withme/global/config/JpaAuditingConfig.java
+++ server/src/main/java/com/ssafy/withme/global/config/JpaAuditingConfig.java
@@ -1,4 +1,4 @@
-package com.ssafy.withme.config;
+package com.ssafy.withme.global.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
