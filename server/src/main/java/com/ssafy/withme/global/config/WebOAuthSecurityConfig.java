885b00cb03c8fe18dc5c560e5d0bcff2ed073635
jinwoo <zvzv9808@gmail.com>
2024-07-19 14:17:09 +0900

    refactor: jpa auditing config 수정

--- server/src/main/java/com/ssafy/withme/global/config/WebOAuthSecurityConfig.java
+++ server/src/main/java/com/ssafy/withme/global/config/WebOAuthSecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -21,6 +22,7 @@ import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @RequiredArgsConstructor
+@EnableWebSecurity
 @Configuration
 public class WebOAuthSecurityConfig {
 
