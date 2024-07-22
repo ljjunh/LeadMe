0857fcce1d531818374b6abd0876f944ef96c927
sgo722@naver.com <sgo722@naver.com>
2024-07-22 17:20:17 +0900

    fix: 파이썬 환경설정

---
+++ server/src/main/java/com/ssafy/withme/controller/userchallenege/UserChallengeController.java
@@ -0,0 +1,13 @@
+package com.ssafy.withme.controller.userchallenege;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class UserChallengeController {
+
+
+
+
+}
