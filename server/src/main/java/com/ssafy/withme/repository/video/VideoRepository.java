fd97d477dde22b558a624588d77c7af002e86d56
jinwoo <zvzv9808@gmail.com>
2024-07-22 16:09:13 +0900

    refactor: delete repository

--- server/src/main/java/com/ssafy/withme/repository/video/VideoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ssafy.withme.repository.video;
-
-import com.ssafy.withme.domain.video.Video;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VideoRepository extends JpaRepository<Video, Long> {
-}
