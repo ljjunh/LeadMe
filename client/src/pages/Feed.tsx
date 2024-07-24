10be2d1c687f8a41d073817b5003b07b5586d08b
yunhayeon <yhy5049@naver.com>
2024-07-25 08:49:03 +0900

    feat: 헤더 컴포넌트 수정

---
+++ client/src/pages/Feed.tsx
@@ -0,0 +1,12 @@
+import Header from "../components/Header";
+
+const Feed = () => {
+  return (
+    <>
+      <Header stickyOnly />
+      <div>Feed</div>
+    </>
+  );
+};
+
+export default Feed;
