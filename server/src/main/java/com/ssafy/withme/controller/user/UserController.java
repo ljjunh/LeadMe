f7af915e73d1ee4c92ee2da8b2ffe7c47e41affa
jinwoo <zvzv9808@gmail.com>
2024-07-25 14:56:49 +0900

    build test

--- server/src/main/java/com/ssafy/withme/controller/user/UserController.java
+++ server/src/main/java/com/ssafy/withme/controller/user/UserController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     private final UserService userService;
-    // build
+
     @GetMapping("/info/{id}")
     public SuccessResponse<UserInfoDto> findUser(@PathVariable Long id) {
 
