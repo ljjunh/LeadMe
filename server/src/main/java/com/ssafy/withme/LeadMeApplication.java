fe71e52dc8f931e2c0a961a7e18dabc634696e9e
nbo5548 <nbo5548@naver.com>
2024-07-25 10:00:01 +0900

    refactor: refactoring

--- server/src/main/java/com/ssafy/withme/LeadMeApplication.java
+++ server/src/main/java/com/ssafy/withme/LeadMeApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WithmeApplication {
+public class LeadMeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WithmeApplication.class, args);
+		SpringApplication.run(LeadMeApplication.class, args);
 	}
 
 }
--- server/src/main/java/com/ssafy/withme/global/config/WebConfig.java
+++ server/src/main/java/com/ssafy/withme/global/config/WebConfig.java
@@ -7,7 +7,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @ComponentScan("com.ssafy.withme")
-public class WebConfig implements WebMvcConfigurer {
+public class
+WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
 
