e765d5a294b58d490d7a739ded18aae5c58673a5
Jinwoo <zvzv9808@gmail.com>
2024-07-28 22:57:07 +0900

    refactor: kakao login user data field 추가

--- server/src/main/java/com/ssafy/withme/domain/dto/KakaoResponse.java
+++ server/src/main/java/com/ssafy/withme/domain/dto/KakaoResponse.java
@@ -1,6 +1,7 @@
 package com.ssafy.withme.domain.dto;
 
 import com.ssafy.withme.domain.user.User;
+import com.ssafy.withme.domain.user.constant.RoleType;
 import com.ssafy.withme.domain.user.constant.UserStatus;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,12 +12,18 @@ import java.util.Map;
 public class KakaoResponse implements OAuth2Response{
 
     private final Map<String, Object> attributes;
+    private final Map<String, Object> kakaoAccount;
+    private final Map<String, Object> profile;
 
     public KakaoResponse(Map<String, Object> attributes) {
         this.attributes = attributes;
+
         System.out.println("??들어오나");
         System.out.println(attributes);
+
         log.debug("attribute: {}", attributes.get("kakao_account"));
+        this.kakaoAccount = (Map<String, Object>) attributes.get("kakao_account");
+        this.profile = (Map<String, Object>) kakaoAccount.get("profile");
     }
 
     @Override
@@ -31,14 +38,12 @@ public class KakaoResponse implements OAuth2Response{
 
     @Override
     public String getEmail() {
-        Map<String, Object> properties = (Map<String, Object>) attributes.get("kakao_account");
-        return properties.get("email").toString();
+        return kakaoAccount.get("email").toString();
     }
 
     @Override
     public String getName() {
-        Map<String, Object> properties = (Map<String, Object>) attributes.get("properties");
-        return properties.get("nickname").toString();
+        return kakaoAccount.get("nickname").toString();
     }
 
     public User toEntity() {
@@ -50,6 +55,9 @@ public class KakaoResponse implements OAuth2Response{
                 .email(email)
                 .name(name)
                 .userStatus(UserStatus.ACTIVE)
+                .profileImg(kakaoAccount.get("profile_image_url").toString())
+                .nickname(profile.get("nickname").toString())
+                .roleType(RoleType.USER)
                 .build();
     }
 }
