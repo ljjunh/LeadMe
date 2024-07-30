07d8cab5379c81ba70e9f356093c4ef0ec16c577
jinwoo <zvzv9808@gmail.com>
2024-07-30 14:39:02 +0900

    docs: remove comment

--- server/src/main/java/com/ssafy/withme/global/config/WebOAuthSecurityConfig.java
+++ server/src/main/java/com/ssafy/withme/global/config/WebOAuthSecurityConfig.java
@@ -30,104 +30,104 @@ import java.util.List;
 @Configuration
 public class WebOAuthSecurityConfig {
 //
-//    private final OAuth2UserCustomService oAuth2UserCustomService;
-//    private final TokenProvider tokenProvider;
-//    private final RefreshTokenRepository refreshTokenRepository;
-//    private final UserService userService;
-//
-//    // 스프링 시큐리티 기능 비활성화
-//    @Bean
-//    public WebSecurityCustomizer configure() {
-//        return (web) -> web.ignoring()
-//                .requestMatchers("/img/**", "/css/**", "/js/**");
-//
-//    }
-//
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        return http
-//                .csrf(AbstractHttpConfigurer::disable)
-//                .httpBasic(AbstractHttpConfigurer::disable)
-//                .formLogin(AbstractHttpConfigurer::disable)
-//                .logout(AbstractHttpConfigurer::disable)
-//                .sessionManagement(mangement -> mangement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-//
-//                .authorizeRequests(auth -> auth
-//                        // 토큰 재발급 url은 인증없이 접근 가능하도록 설정
-//                        .requestMatchers(new AntPathRequestMatcher("/api/token")).permitAll()
-//                        // /api/~ 권한 요규
-//                        .requestMatchers(new AntPathRequestMatcher("/api/**")).authenticated()
-//                        // 이외에는 모두 허가
-//                        //.anyRequest().permitAll()
-//                )
-//
-//                // OAuth 로그인 후 쿠키 세팅 및 유저 레포지토리에 반영
-//                .oauth2Login(oauth2 -> oauth2
-//                        .loginPage("/login")
-//                        .authorizationEndpoint(authorizationEndpoint -> authorizationEndpoint.baseUri("/oauth2/authorization")
-//                                .authorizationRequestRepository(oAuth2AuthorizationRequestBasedOnCookieRepository()))
-//                        //.redirectionEndpoint(endpoint -> endpoint.baseUri("/*/oauth2/code/*"))
-//                        .userInfoEndpoint(userInfoEndPoint -> userInfoEndPoint.userService(oAuth2UserCustomService))
-//                        .successHandler(oAuth2SuccessHandler())
-//                        .failureHandler(oAuth2FailureHandler())
-//
-//                )
-//
-//                // /api로 시작하는 url인 경우 401 상태 코드를 반환하도록 예외 처리
-//                .exceptionHandling(exceptionHandling -> exceptionHandling
-//                        .defaultAuthenticationEntryPointFor(
-//                                new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED),
-//                                new AntPathRequestMatcher("/api/**")
-//                        ))
-//                .build();
-//    }
-//
-//    // 실패 핸들러 예제
-//    private AuthenticationFailureHandler oAuth2FailureHandler() {
-//        return (request, response, exception) -> {
-//            response.sendRedirect("/login?error=" + exception.getStackTrace());
-//        };
-//    }
-//
-//    @Bean
-//    public OAuth2SuccessHandler oAuth2SuccessHandler() {
-//        return new OAuth2SuccessHandler(tokenProvider,
-//                refreshTokenRepository,
-//                oAuth2AuthorizationRequestBasedOnCookieRepository(),
-//                userService
-//        );
-//    }
-//
-//
-//    @Bean
-//    public TokenAuthenticationFilter tokenAuthenticationFilter() {
-//        return new TokenAuthenticationFilter(tokenProvider);
-//    }
-//
-//
-//    @Bean
-//    public OAuth2AuthorizationRequestBasedOnCookieRepository oAuth2AuthorizationRequestBasedOnCookieRepository() {
-//        return new OAuth2AuthorizationRequestBasedOnCookieRepository();
-//    }
-//
-//
-//    @Bean
-//    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
+    private final OAuth2UserCustomService oAuth2UserCustomService;
+    private final TokenProvider tokenProvider;
+    private final RefreshTokenRepository refreshTokenRepository;
+    private final UserService userService;
+
+    // 스프링 시큐리티 기능 비활성화
+    @Bean
+    public WebSecurityCustomizer configure() {
+        return (web) -> web.ignoring()
+                .requestMatchers("/img/**", "/css/**", "/js/**");
+
+    }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests((requests) -> requests
-                        .anyRequest().permitAll()
-                )
-                .csrf((csrf) -> csrf.disable())
-                .httpBasic((httpBasic) -> httpBasic.disable())
-                .formLogin((formLogin) -> formLogin.disable());
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .logout(AbstractHttpConfigurer::disable)
+                .sessionManagement(mangement -> mangement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 
-        return http.build();
+                .authorizeRequests(auth -> auth
+                        // 토큰 재발급 url은 인증없이 접근 가능하도록 설정
+                        .requestMatchers(new AntPathRequestMatcher("/api/token")).permitAll()
+                        // /api/~ 권한 요규
+                        .requestMatchers(new AntPathRequestMatcher("/api/**")).authenticated()
+                        // 이외에는 모두 허가
+                        //.anyRequest().permitAll()
+                )
+
+                // OAuth 로그인 후 쿠키 세팅 및 유저 레포지토리에 반영
+                .oauth2Login(oauth2 -> oauth2
+                        .loginPage("/login")
+                        .authorizationEndpoint(authorizationEndpoint -> authorizationEndpoint.baseUri("/oauth2/authorization")
+                                .authorizationRequestRepository(oAuth2AuthorizationRequestBasedOnCookieRepository()))
+                        //.redirectionEndpoint(endpoint -> endpoint.baseUri("/*/oauth2/code/*"))
+                        .userInfoEndpoint(userInfoEndPoint -> userInfoEndPoint.userService(oAuth2UserCustomService))
+                        .successHandler(oAuth2SuccessHandler())
+                        .failureHandler(oAuth2FailureHandler())
+
+                )
+
+                // /api로 시작하는 url인 경우 401 상태 코드를 반환하도록 예외 처리
+                .exceptionHandling(exceptionHandling -> exceptionHandling
+                        .defaultAuthenticationEntryPointFor(
+                                new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED),
+                                new AntPathRequestMatcher("/api/**")
+                        ))
+                .build();
     }
+
+    // 실패 핸들러 예제
+    private AuthenticationFailureHandler oAuth2FailureHandler() {
+        return (request, response, exception) -> {
+            response.sendRedirect("/login?error=" + exception.getStackTrace());
+        };
+    }
+
+    @Bean
+    public OAuth2SuccessHandler oAuth2SuccessHandler() {
+        return new OAuth2SuccessHandler(tokenProvider,
+                refreshTokenRepository,
+                oAuth2AuthorizationRequestBasedOnCookieRepository(),
+                userService
+        );
+    }
+
+
+    @Bean
+    public TokenAuthenticationFilter tokenAuthenticationFilter() {
+        return new TokenAuthenticationFilter(tokenProvider);
+    }
+
+
+    @Bean
+    public OAuth2AuthorizationRequestBasedOnCookieRepository oAuth2AuthorizationRequestBasedOnCookieRepository() {
+        return new OAuth2AuthorizationRequestBasedOnCookieRepository();
+    }
+
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeHttpRequests((requests) -> requests
+//                        .anyRequest().permitAll()
+//                )
+//                .csrf((csrf) -> csrf.disable())
+//                .httpBasic((httpBasic) -> httpBasic.disable())
+//                .formLogin((formLogin) -> formLogin.disable());
+//
+//        return http.build();
+//    }
 
     @Bean
     public CorsFilter corsFilter() {
