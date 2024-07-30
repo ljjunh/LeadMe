084de585480d1d425b84a011d2a3365c43effaa7
sgo722@naver.com <sgo722@naver.com>
2024-07-30 09:31:58 +0900

    feat : 유저 댓글 수정 및 삭제 기능 권한에 따른 예외처리기능

--- server/src/main/java/com/ssafy/withme/controller/comment/request/CommentUpdateRequest.java
+++ server/src/main/java/com/ssafy/withme/controller/comment/request/CommentUpdateRequest.java
@@ -7,6 +7,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Getter
 public class CommentUpdateRequest {
+
+    private Long userId;
+
     private Long commentId;
 
     private String content;
